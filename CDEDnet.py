ae5ff540565462e16745f42f7e7a3f9d0cb4c6ec
Ashcarnage <86963238+Ashcarnage@users.noreply.github.com>
2023-09-06 15:38:30 +0530

    Update CDEDnet.py

--- CDEDnet.py
+++ CDEDnet.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf 
 from tensorflow import keras
 from keras.models import Sequential,Model
-from keras.layers import Input,Activation, Dense,Flatten, BatchNormalization,Conv2D,MaxPool2D,UpSampling2D,Concatenate
+from keras.layers import Input,Conv2DTranspose,Activation, Dense,Flatten, BatchNormalization,Conv2D,MaxPool2D,UpSampling2D,Concatenate
 from keras.optimizers import Adam
 from keras.metrics import categorical_crossentropy
 import os
@@ -13,7 +13,7 @@ import imghdr
 data = tf.keras.utils.image_dataset_from_directory('data',batch_size=5,image_size=(500,560)) 
 data_dir="data"
 data = data.map(lambda x,y: (x/255,y))
-data_iterator = data.as_numpy_iterator().next()
+#data_iterator = data.as_numpy_iterator().next()
 # batch  =  data_iterator.next()
 # print(batch[0].shape)
 #print(len(data))
@@ -32,89 +32,89 @@ class Encoder():
         self.model = None
         self.toggle = True
         self.encdr_features = {}
+        self.filters=64
         self.build()
         
-    def conv_block(self,inputs,filters,pool = True):
-        x = Conv2D(filters,(3,3),1,padding = "same",activation="relu")(inputs)
-        x = Conv2D(filters,(3,3),1,padding = "same",activation="relu")(x)
+    def conv_block(self,inputs,filters= None,pool = True):
+        if filters!=None:
+            self.filters = filters
+        x = Conv2D(self.filters,(3,3),1,padding = "same",activation="relu")(inputs)
+        if filters==None : self.filters*=2
+        if self.toggle : 
+            self.filters = 64
+            self.toggle = False
+        x = Conv2D(self.filters,(3,3),1,padding = "same",activation="relu")(x)
         if pool:
             p = MaxPool2D(pool_size=(2,2),strides=2)(x)
             return x,p
         else:
             return x
-        
-    
     def build(self):
-        input_size = (500,560,3)
+        input_size = (500,560,1)
         inputs = Input(input_size)
-        n = 64
-        x,p = self.conv_block(inputs,n)
+        x,p = self.conv_block(inputs,filters=1)
         self.encdr_features["x1"] = x
         for i in range(3):
-            x,p = self.conv_block(p,n*2)
+            x,p = self.conv_block(p)
             input_size = p
             self.encdr_features[f"x{i+2}"] = x
-            n*=2
+        self.encdr_features[f"x{i+3}"]=p
         self.model = Model(inputs=inputs,outputs = p)
         self.model.summary()
-        for i in self.encdr_features.values():
-            print(i)
-
-            
-
         # bridge 
 
-class Decoder(Encoder):
-    def __init__(self,feature_maps):
-        self.model = None
+class Decoder():
+    def __init__(self,conv_block,feature_maps):
+        self.conv_block = conv_block
+        self.toggle = False
         self.feature_maps = feature_maps
-        self.x1 = list(self.feature_maps.keys())[-1]
-        self.x_size = self.feature_maps[self.x1].shape
-        self.x2 = None
         self.build()
-    
-    def upsampling_block(self,filters):
-        u1 = UpSampling2D((3,3),interpolation = "lanczos3")(self.feature_maps[self.x1])
-        print("life saver : ",self.feature_maps[self.x2].shape)
-        c1 = Concatenate()([u1,self.feature_maps[self.x2]])
-        x0 = self.conv_block(c1,filters,pool = False)
-        x5 = self.conv_block(x0,filters//2,pool = False)
-        return x5
+    def decoder_block(self,inp,filters,concat_layer):
+        print(inp)
+        x = Conv2DTranspose(filters,(3,3),strides=(2,2),padding="same",activation="relu")(inp)
+        print("AFTERRRRR DARKK ; ", x)
+        print(x.shape[1])
+        if self.toggle:
+            pad_height = 1 - tf.shape(x)[1] % 2  # Calculate how much padding is needed to make it odd
+            x = tf.pad(x, [[0, 0], [0, pad_height], [0, 0], [0, 0]])
+            print("squuuuuuuuu :",x)
+            self.toggle = False
+        x = Concatenate(axis = -1)([x,concat_layer]) 
+        x = self.conv_block(x,filters,pool = False)
+        print("finalllllllllylyyyyyy",x)
 
+
+        print("---------------------------------------> sex")
+        return x
     def build(self):
-        filters = 512
-        inputs = Input(self.x_size)
-        for i,x in enumerate(list(self.feature_maps.keys())[-2::-1]):
-            self.x2 = x
-            x0 = self.upsampling_block(filters)
-            self.feature_maps[self.x1] = x0
-            print("done done")
-            filters//=2
-        self.model = Model(inputs = inputs,outputs = x0)
-        self.model.summary()
+        inputs = Input((500,560,1))
+        x1 = self.feature_maps[list(self.feature_maps.keys())[-1]]
+        filter = 256
+        print("oooolalalalala : ",x1)
+        a = True
+        for x in list(self.feature_maps.keys())[-2::-1]:
+            print("oollayyayayayayayayay",self.feature_maps[x])
+            x1 = self.decoder_block(x1,filter,self.feature_maps[x])
+            if filter ==64:
+                filter = 3
+            filter//=2
+            if a :
+                self.toggle = True
+                a = False
+        outputs = x1
+        ml = Model(inputs,outputs)
+        #print(ml.summary())
 
 
 
 
 
-        
+
+
 
 
 #print(Input((500,560,3)))
 Encdr = Encoder(train)
-Decdr = Decoder(Encdr.encdr_features)
-
-
-
-
-
-
-
-
-
-
-
-
-
+Decdr = Decoder(Encdr.conv_block,Encdr.encdr_features)
 
 
